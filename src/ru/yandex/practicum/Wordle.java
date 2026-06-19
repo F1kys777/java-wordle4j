ff880f339aa12201e163703db125c5a6ffe6f797
fikys <maximfeok2200@gmail.com>
2026-06-19 18:25:14 +0300

    Начало работ

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum;
 
+import java.util.Scanner;
+
 /*
 в главном классе нам нужно:
     создать лог-файл (он должен передаваться во все классы)
@@ -11,8 +13,35 @@ package ru.yandex.practicum;
  */
 public class Wordle {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        printMenu();
+
+        while (true) {
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Запускаем игру...");
+                    break;
+                case 2:
+                    System.out.println("Выход из игры, досвидания!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Неверный выбор! Вы можете начать игру или выйти из игры.");
+            }
+        }
     }
 
+    public static void printMenu() {
+
+        System.out.println("\n=== ГЛАВНОЕ МЕНЮ ===");
+        System.out.println("1. Начать игру");
+        System.out.println("2. Выход");
+
+    }
 }
--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,9 +1,42 @@
 package ru.yandex.practicum;
 
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
     ему нужны методы по загрузке списка слов из файла по имени файла
     на выходе должен быть класс WordleDictionary
- */
+*/
 public class WordleDictionaryLoader {
+    private static final String connectDictionary = "C:\\Users\\user\\Desktop\\код\\java-wordle4j";
+
+    try(BufferedReader reader = new BufferedReader(
+            (new FileReader("words_ru.txt", StandardCharsets.UTF_8))) {
+        while(reader.ready()) {
+            String line = reader.readLine();
+            Path filteredDictionary = Paths.get(connectDictionary, "filteredDictionaryFile.txt")
+            if(line.length() == 5) {
+                try (Writer fileWriter = new FileWriter("filteredDictionaryFile", true)){
+                    fileWriter.write(line + "\n");
+                } catch (IIOException e) {
+                    e.printStackTrace();
+                } finally {
+                    fileWriter.close();
+                }
+            } else {
+                continue;
+            }
+        }
+    } catch {
+
+    }
+
 }
